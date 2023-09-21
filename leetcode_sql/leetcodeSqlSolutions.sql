681a793fa07a84f9ef68e5f2161aeadf2fe4ee67
Santosh Sahni <meet.santosh96@gmail.com>
2023-09-21 08:36:42 +0530

    second highest salary

--- leetcode_sql/leetcodeSqlSolutions.sql
+++ leetcode_sql/leetcodeSqlSolutions.sql
@@ -1,6 +1,9 @@
 -- https://leetcode.com/problems/second-highest-salary/description/
 select max(salary) as "SecondHighestSalary" from employee where salary < (select max(salary) from employee);
 
+-- 2nd way
+select ifnull((select distinct salary from employee order by salary desc limit 1,1), null) as SecondHighestSalary;
+
 -- https://leetcode.com/problems/employees-earning-more-than-their-managers/
 select e.name as Employee from employee e, employee m where m.id=e.managerId and e.salary > m.salary;
 
@@ -17,3 +20,4 @@ FETCH FIRST x ROWS ONLY;
 FETCH FIRST x PERCENT ROWS ONLY
 ROWNUM >=< x
 */
+

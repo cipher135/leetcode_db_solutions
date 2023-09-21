8e8d3bfd0b99e2af8e15387c8a6dcd0ea3281e37
Santosh Sahni <meet.santosh96@gmail.com>
2023-09-21 07:47:20 +0530

    SQL

---
+++ leetcode_sql/leetcodeSqlSolutions.sql
@@ -0,0 +1,19 @@
+-- https://leetcode.com/problems/second-highest-salary/description/
+select max(salary) as "SecondHighestSalary" from employee where salary < (select max(salary) from employee);
+
+-- https://leetcode.com/problems/employees-earning-more-than-their-managers/
+select e.name as Employee from employee e, employee m where m.id=e.managerId and e.salary > m.salary;
+
+/*
+> MSSQL server and MS Access database
+SELECT TOP x
+SELECT TOP x PERCENT
+
+> MySQL
+LIMIT x
+
+> Oracle (at the end of the query)     
+FETCH FIRST x ROWS ONLY;
+FETCH FIRST x PERCENT ROWS ONLY
+ROWNUM >=< x
+*/
